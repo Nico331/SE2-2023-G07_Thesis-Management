778868ac684dd56f06b267af6d163a5d360c693e
teogra00 <matteogravile00@gmail.com>
2023-11-16 12:57:42 +0100

    ADD basic APIs for Career and Degree

--- server/src/main/kotlin/it/polito/server/career/CareerService.kt
+++ server/src/main/kotlin/it/polito/server/career/CareerService.kt
@@ -1,8 +1,45 @@
 package it.polito.server.career
 
+import it.polito.server.student.Student
+import it.polito.server.student.StudentDTO
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
 import org.springframework.stereotype.Service
+import org.springframework.web.bind.annotation.DeleteMapping
+import org.springframework.web.bind.annotation.PathVariable
 
 @Service
 class CareerService (private val careerRepository: CareerRepository) {
+
+    fun createCareer(career: Career): CareerDTO {
+        val savedCareer = careerRepository.save(career)
+        return savedCareer.toDTO()
+    }
+
+    fun findCareerById(id: String): CareerDTO? {
+        return careerRepository.findById(id).map(Career::toDTO).orElse(null)
+    }
+
+    fun allCareers(): List<CareerDTO> {
+        return careerRepository.findAll().map { it.toDTO() }
+    }
+
+    fun updateCareer(id: String, update: CareerDTO): CareerDTO? {
+        val career = careerRepository.findById(id).orElse(null) ?: return null
+
+        career.studentId = update.studentId
+        career.codCourse = update.codCourse
+        career.titleCourse = update.titleCourse
+        career.cfu = update.cfu
+        career.grade = update.grade
+        career.date = update.date
+
+
+        return careerRepository.save(career).toDTO()
+    }
+
+    fun deleteCareer(id: String) {
+        careerRepository.deleteById(id)
+    }
 
 }
--- server/src/main/kotlin/it/polito/server/degree/DegreeController.kt
+++ server/src/main/kotlin/it/polito/server/degree/DegreeController.kt
@@ -1,4 +1,39 @@
 package it.polito.server.degree
 
-class DegreeController {
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.*
+
+@RestController
+@RequestMapping("/API/Degree")
+class DegreeController (private val degreeService: DegreeService) {
+
+    @PostMapping("")
+    fun createDegree(@RequestBody degree: Degree): ResponseEntity<DegreeDTO> {
+        val newDegree = degreeService.createDegree(degree)
+        return ResponseEntity(newDegree, HttpStatus.CREATED)
+    }
+
+    @GetMapping("/{id}")
+    fun getDegree(@PathVariable id: String): ResponseEntity<DegreeDTO> {
+        val degree = degreeService.findDegreeById(id) ?: return ResponseEntity(HttpStatus.NOT_FOUND)
+        return ResponseEntity.ok(degree)
+    }
+
+    @GetMapping
+    fun allDegree(): List<DegreeDTO> {
+        return degreeService.allDegrees()
+    }
+
+    @PutMapping("/{id}")
+    fun updateDegree(@PathVariable id: String, @RequestBody update: DegreeDTO): ResponseEntity<DegreeDTO> {
+        val updatedDegree = degreeService.updateDegree(id, update) ?: return ResponseEntity(HttpStatus.NOT_FOUND)
+        return ResponseEntity.ok(updatedDegree)
+    }
+
+    @DeleteMapping("/{id}")
+    fun deleteDegree(@PathVariable id: String): ResponseEntity<Void> {
+        degreeService.deleteDegree(id)
+        return ResponseEntity(HttpStatus.OK)
+    }
 }
