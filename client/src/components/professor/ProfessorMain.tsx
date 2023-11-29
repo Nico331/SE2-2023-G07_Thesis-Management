1c650b16b2786b7735ee60491673a0a52c7a8931
filippobertolotti0 <filippobertolotti0@gmail.com>
2023-11-29 13:10:49 +0100

    fix some little aspects

--- client/src/components/professor/ProfessorMain.tsx
+++ client/src/components/professor/ProfessorMain.tsx
@@ -14,8 +14,8 @@ const ProfessorMain= () => {
                 <h1>Welcome to Polito thesis management system</h1>
             </Container>
             <Container className="mt-3 d-flex flex-column align-items-center">
-                <Button className="mt-5 fs-3" variant="outline-secondary" style={{borderRadius:"30px", width:"400px", height:"60px"}} onClick={() => navigate("/add-proposal")}>Add a new thesis proposal</Button>
-                <Button className="mt-5 fs-3" variant="outline-secondary" style={{borderRadius:"30px", width:"400px", height:"60px"}} onClick={() => navigate("/browse-applications")}>My thesis proposals</Button>
+                <Button className="mt-5 fs-3" variant="outline-primary" style={{borderRadius:"30px", width:"400px", height:"60px"}} onClick={() => navigate("/add-proposal")}>Add a new thesis proposal</Button>
+                <Button className="mt-5 fs-3" variant="outline-primary" style={{borderRadius:"30px", width:"400px", height:"60px"}} onClick={() => navigate("/browse-applications")}>My thesis proposals</Button>
             </Container>
             <Container className="d-flex justify-content-center" style={{marginTop:"15vh"}}>
                 <Image style={{ width: 350, height: 150 }} src={"../logoPolito.png"}/>
