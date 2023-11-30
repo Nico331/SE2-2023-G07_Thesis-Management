2b611198bb897e436b9a41e0abd5daca316fb8fb
Nico331 <slav.nicolai@mail.ru>
2023-11-30 13:20:45 +0100

    quick fix

--- server/src/main/kotlin/it/polito/server/proposal/ProposalService.kt
+++ server/src/main/kotlin/it/polito/server/proposal/ProposalService.kt
@@ -39,7 +39,8 @@ class ProposalService (private val proposalRepository : ProposalRepository,
         return proposalRepository.findById(id).map(Proposal::toDTO).orElse(null)
     }
     fun findAll() : List<ProposalDTO> {
-        return proposalRepository.findAll().map{(it.toDTO())}
+        return proposalRepository.findByArchived(archiviation_type.NOT_ARCHIVED).map{(it.toDTO())}
+        //return proposalRepository.findAll().map{(it.toDTO())}
     }
 
     fun findActiveByStudent( studentId : String): List<ProposalDTO>? {
