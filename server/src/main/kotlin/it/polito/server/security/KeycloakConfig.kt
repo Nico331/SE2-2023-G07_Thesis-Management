3c5ea34e258d28d397cb7105e1bf1323af51df8d
Nico331 <slav.nicolai@mail.ru>
2023-12-08 13:35:17 +0100

    removed keycloak config, controller and service

--- server/src/main/kotlin/it/polito/server/security/KeycloakConfig.kt
+++ server/src/main/kotlin/it/polito/server/security/KeycloakConfig.kt
@@ -11,41 +11,41 @@ import org.springframework.boot.context.properties.ConfigurationProperties
 import org.springframework.context.annotation.Bean
 import org.springframework.stereotype.Component
 import java.io.File
-
-@Component
-object KeycloakConfig {
-    private var keycloak: Keycloak? = null
-    private var serverUrl = if (isRunningInDockerContainer()){
-        "http://custom_keycloak:8080"
-    }else{
-        "http://localhost:8082"
-    }
-    var realm = "ThesisManagementRealm"
-    private var clientId = "springboot-keycloak-client"
-    private var password = "admin"
-    private var username = "password"
-    fun getInstance(): Keycloak {
-        if (keycloak == null) {
-            keycloak = KeycloakBuilder.builder()
-                .serverUrl(serverUrl)
-                .realm(realm)
-                .grantType(OAuth2Constants.PASSWORD)
-                .username(username)
-                .password(password)
-                .clientId(clientId)
-                //.clientSecret(clientSecret)
-                .resteasyClient(
-                    ResteasyClientBuilderImpl()
-                        .connectionPoolSize(10)
-                        .build()
-                )
-                .build()
-        }
-        return keycloak!!
-    }
-
-    fun isRunningInDockerContainer(): Boolean {
-        val cgroupFile = File("/proc/self/cgroup")
-        return cgroupFile.exists() && cgroupFile.readText().contains("docker")
-    }
-}
+//
+//@Component
+//object KeycloakConfig {
+//    private var keycloak: Keycloak? = null
+//    private var serverUrl = if (isRunningInDockerContainer()){
+//        "http://custom_keycloak:8080"
+//    }else{
+//        "http://localhost:8082"
+//    }
+//    var realm = "ThesisManagementRealm"
+//    private var clientId = "springboot-keycloak-client"
+//    private var password = "password"
+//    private var username = "admin"
+//    fun getInstance(): Keycloak {
+//        if (keycloak == null) {
+//            keycloak = KeycloakBuilder.builder()
+//                .serverUrl(serverUrl)
+//                .realm(realm)
+//                .grantType(OAuth2Constants.PASSWORD)
+//                .username(username)
+//                .password(password)
+//                .clientId(clientId)
+//                //.clientSecret(clientSecret)
+//                .resteasyClient(
+//                    ResteasyClientBuilderImpl()
+//                        .connectionPoolSize(10)
+//                        .build()
+//                )
+//                .build()
+//        }
+//        return keycloak!!
+//    }
+//
+//    fun isRunningInDockerContainer(): Boolean {
+//        val cgroupFile = File("/proc/self/cgroup")
+//        return cgroupFile.exists() && cgroupFile.readText().contains("docker")
+//    }
+//}
