31da506ecd921d744d7e59777b86e96bbbcf6f26
filippobertolotti0 <filippobertolotti0@gmail.com>
2023-12-12 10:46:58 +0100

    add archive into the menu

--- client/src/components/NavBar.tsx
+++ client/src/components/NavBar.tsx
@@ -68,6 +68,16 @@ export default function MainNavBar ({role, user, setRole}) {
                                                     </Nav.Link>
                                                 </Row>
                                                 <Row>
+                                                    <Nav.Link href="/archived-proposals" >
+                                                        <Container className="p-0 m-0 d-inline-flex align-items-center">
+                                                            <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" fill="currentColor" className="bi bi-archive" viewBox="0 0 16 16">
+                                                                <path d="M0 2a1 1 0 0 1 1-1h14a1 1 0 0 1 1 1v2a1 1 0 0 1-1 1v7.5a2.5 2.5 0 0 1-2.5 2.5h-9A2.5 2.5 0 0 1 1 12.5V5a1 1 0 0 1-1-1zm2 3v7.5A1.5 1.5 0 0 0 3.5 14h9a1.5 1.5 0 0 0 1.5-1.5V5zm13-3H1v2h14zM5 7.5a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1-.5-.5"/>
+                                                            </svg>
+                                                            <p className="ms-3 my-0 fs-3">Archive</p>
+                                                        </Container>
+                                                    </Nav.Link>
+                                                </Row>
+                                                <Row>
                                                     <Nav.Link href="/add-proposal">
                                                         <Container className="p-0 m-0 d-inline-flex align-items-center">
                                                             <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" fill="currentColor" className="bi bi-plus-lg" viewBox="0 0 16 16">
--- client/src/components/professor/ProfessorMain.tsx
+++ client/src/components/professor/ProfessorMain.tsx
@@ -9,15 +9,15 @@ const ProfessorMain= () => {
 
     return (
         <Container className="d-flex flex-column" style={{height:"100vh"}}>
-            <Container className="d-flex flex-column justify-content-between" style={{marginTop:"120px", height:"100%", paddingTop:"10px", paddingBottom:"80px"}}>
+            <Container className="d-flex flex-column justify-content-between" style={{marginTop:"120px", height:"100%", paddingBottom:"40px"}}>
                 <Container className="text-center">
                     <h1>Hi {user.name}!</h1>
                     <h1>Welcome to Polito thesis management system</h1>
                 </Container>
                 <Container className="d-flex flex-column align-items-center">
                     <Button className="fs-3" variant="outline-primary" style={{borderRadius:"30px", width:"400px", height:"60px"}} onClick={() => navigate("/add-proposal")}>Add a new thesis proposal</Button>
-                    <Button className="mt-5 fs-3" variant="outline-primary" style={{borderRadius:"30px", width:"400px", height:"60px"}} onClick={() => navigate("/browse-applications")}>My thesis proposals</Button>
-                    <Button className="mt-5 fs-3" variant="outline-primary" style={{borderRadius:"30px", width:"400px", height:"60px"}} onClick={() => navigate("/archived-proposals")}>My archived thesis proposals</Button>
+                    <Button className="mt-4 fs-3" variant="outline-primary" style={{borderRadius:"30px", width:"400px", height:"60px"}} onClick={() => navigate("/browse-applications")}>My thesis proposals</Button>
+                    <Button className="mt-4 fs-3" variant="outline-primary" style={{borderRadius:"30px", width:"400px", height:"60px"}} onClick={() => navigate("/archived-proposals")}>My archived thesis proposals</Button>
                 </Container>
                 <Container className="d-flex justify-content-center">
                     <Image style={{ width: 400, height: 167 }} src={"../logoPolito.png"}/>
