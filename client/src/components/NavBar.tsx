f6bc659bdc184073ed8fc0c0e00d8d099ff8fb9f
Soheil Jamshidi <eris1376@gmail.com>
2023-12-12 15:32:53 +0100

    Bring back the Menu of student

--- client/src/components/NavBar.tsx
+++ client/src/components/NavBar.tsx
@@ -87,6 +87,42 @@ export default function MainNavBar ({role, user, setRole}) {
                                                 </Row>
                                             </Row>
                                         </>): null}
+                                        
+                                        {role === "STUDENT" ? ( <>
+                                            <Row className='canvas'>
+                                                <Row>
+                                                    <Nav.Link href="/proposalList" >
+                                                        <Container className="p-0 m-0 d-inline-flex align-items-center">
+                                                            <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" fill="currentColor" className="bi bi-search" viewBox="0 0 16 16">
+                                                                <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0"/>
+                                                            </svg>
+                                                            <p className="ms-3 my-0 fs-3">Search proposals</p>
+                                                        </Container>
+                                                    </Nav.Link>
+                                                </Row>
+                                                <Row>
+                                                    <Nav.Link href="/myApplicationList" >
+                                                        <Container className="p-0 m-0 d-inline-flex align-items-center">
+                                                            <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" fill="currentColor" className="bi bi-check-lg" viewBox="0 0 16 16">
+                                                                <path d="M12.736 3.97a.733.733 0 0 1 1.047 0c.286.289.29.756.01 1.05L7.88 12.01a.733.733 0 0 1-1.065.02L3.217 8.384a.757.757 0 0 1 0-1.06.733.733 0 0 1 1.047 0l3.052 3.093 5.4-6.425a.247.247 0 0 1 .02-.022"/>
+                                                            </svg>
+                                                            <p className="ms-3 my-0 fs-3">My Applications</p>
+                                                        </Container>
+                                                    </Nav.Link>
+                                                </Row>
+                                                <Row>
+                                                    <Nav.Link href="/logout">
+                                                        <Container className="p-0 m-0 d-inline-flex align-items-center">
+                                                            <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" fill="currentColor" className="bi bi-box-arrow-right" viewBox="0 0 16 16">
+                                                                <path fillRule="evenodd" d="M10 12.5a.5.5 0 0 1-.5.5h-8a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5h8a.5.5 0 0 1 .5.5v2a.5.5 0 0 0 1 0v-2A1.5 1.5 0 0 0 9.5 2h-8A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h8a1.5 1.5 0 0 0 1.5-1.5v-2a.5.5 0 0 0-1 0z"/>
+                                                                <path fillRule="evenodd" d="M15.854 8.354a.5.5 0 0 0 0-.708l-3-3a.5.5 0 0 0-.708.708L14.293 7.5H5.5a.5.5 0 0 0 0 1h8.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3z"/>
+                                                            </svg>
+                                                            <p className="ms-3 my-0 fs-3">Logout</p>
+                                                        </Container>
+                                                    </Nav.Link>
+                                                </Row>
+                                            </Row>
+                                        </>): null}
 
                                 {role === "ADMIN" ? ( <>
                                 <Row className='canvas'>
