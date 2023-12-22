c228f60bd897e5782fd4dc30158a2269a6be0f90
Nico331 <slav.nicolai@mail.ru>
2023-12-23 00:13:36 +0100

    fix:  - try catch when email server is not reachable

--- server/src/main/kotlin/it/polito/server/email/EmailService.kt
+++ server/src/main/kotlin/it/polito/server/email/EmailService.kt
@@ -3,17 +3,23 @@ package it.polito.server.email
 import org.springframework.mail.SimpleMailMessage
 import org.springframework.mail.javamail.JavaMailSender
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.mail.MailException
 import org.springframework.stereotype.Service
 
 @Service
 class EmailService(@Autowired private val emailSender: JavaMailSender) {
 
     fun sendSimpleMessage(to: String, subject: String, text: String, from: String) {
-        val message = SimpleMailMessage()
-        message.from = from
-        message.setTo(to)
-        message.subject = subject
-        message.text = text
-        emailSender.send(message)
+        try {
+            val message = SimpleMailMessage()
+            message.from = from
+            message.setTo(to)
+            message.subject = subject
+            message.text = text
+            emailSender.send(message)
+        } catch (e: MailException) {
+            // Log dell'errore
+            println("Errore nell'invio della mail a $to: ${e.message}")
+        }
     }
 }
