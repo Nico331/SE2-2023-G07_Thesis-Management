9a67e6d261dc74456ed56d02eb0190859e216a50
teogra00 <matteogravile00@gmail.com>
2024-01-03 20:01:50 +0100

    unit test for AppiedProposal to increase test coverage

--- server/src/test/kotlin/it/polito/server/AppliedProposalUnitTests.kt
+++ server/src/test/kotlin/it/polito/server/AppliedProposalUnitTests.kt
@@ -46,6 +46,16 @@ class AppliedProposalUnitTests {
     }
 
     @Test
+    fun testGetAllAppliedProposalsNoContent() {
+        `when`(appliedProposalService.findAll()).thenReturn(emptyList())
+
+        val responseEntity = appliedProposalController.getAll()
+
+        assert(responseEntity.statusCode == HttpStatus.OK)
+        assert(responseEntity.body == emptyList<AppliedProposalDTO>())
+    }
+
+    @Test
     fun testDeleteAppliedProposal() {
         val applicationId = "1"
 
@@ -73,6 +83,82 @@ class AppliedProposalUnitTests {
     }
 
     @Test
+    fun testDeleteAppliedProposalServerError() {
+        val applicationId = "1"
+
+        `when`(appliedProposalService.deleteAppliedProposal(applicationId))
+                .thenReturn(ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Internal Server Error"))
+
+        val responseEntity = appliedProposalController.deleteAppliedProposal(applicationId)
+
+        assert(responseEntity.statusCode == HttpStatus.INTERNAL_SERVER_ERROR)
+        assert(responseEntity.body == "Internal Server Error")
+    }
+
+    @Test
+    fun testDeleteAppliedProposalWithNonPendingStatus() {
+        val applicationId = "1"
+        val nonPendingApplication = AppliedProposalDTO(
+                id = applicationId,
+                proposalId = "1",
+                studentId = "student1",
+                status = ApplicationStatus.ACCEPTED,
+                file = null
+        )
+
+        `when`(appliedProposalService.deleteAppliedProposal(applicationId))
+                .thenReturn(ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Cannot delete application with status ${nonPendingApplication.status}"))
+
+        val responseEntity = appliedProposalController.deleteAppliedProposal(applicationId)
+
+        assert(responseEntity.statusCode == HttpStatus.BAD_REQUEST)
+        assert(responseEntity.body == "Cannot delete application with status ${nonPendingApplication.status}")
+    }
+
+    @Test
+    fun testWithdrawProposal() {
+        val appliedProposalId = "1"
+        val withdrawnAppliedProposal = AppliedProposalDTO(
+                id = appliedProposalId,
+                proposalId = "1",
+                studentId = "student1",
+                status = ApplicationStatus.CANCELLED,
+                file = null
+        )
+        `when`(appliedProposalService.withdrawProposal(appliedProposalId)).thenReturn(ResponseEntity.ok(withdrawnAppliedProposal))
+
+        val responseEntity = appliedProposalController.withdrawProposal(appliedProposalId)
+
+        assert(responseEntity.statusCode == HttpStatus.OK)
+        assert(responseEntity.body == withdrawnAppliedProposal)
+    }
+
+    @Test
+    fun testWithdrawNonExistingProposal() {
+        val nonExistingAppliedProposalId = "99"
+        `when`(appliedProposalService.withdrawProposal(nonExistingAppliedProposalId))
+                .thenReturn(ResponseEntity.status(HttpStatus.NOT_FOUND).body("Application not found"))
+
+        val responseEntity = appliedProposalController.withdrawProposal(nonExistingAppliedProposalId)
+
+        assert(responseEntity.statusCode == HttpStatus.NOT_FOUND)
+        assert(responseEntity.body == "Application not found")
+    }
+
+    @Test
+    fun testWithdrawProposalServerError() {
+        val applicationId = "1"
+
+        `when`(appliedProposalService.withdrawProposal(applicationId))
+                .thenReturn(ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Internal Server Error"))
+
+        val responseEntity = appliedProposalController.withdrawProposal(applicationId)
+
+        assert(responseEntity.statusCode == HttpStatus.INTERNAL_SERVER_ERROR)
+        assert(responseEntity.body == "Internal Server Error")
+    }
+
+    @Test
     fun testApplyForProposal() {
         val proposalId = "1"
         val studentId = "student1"
@@ -94,21 +180,80 @@ class AppliedProposalUnitTests {
         assert(responseEntity.body == appliedProposalDTO)
     }
 
-    /*@Test
-    fun testExistingApplyForProposal(){
+    @Test
+    fun testApplyForProposalBadRequest() {
         val proposalId = "1"
         val studentId = "student1"
         val file = FileDTO(byteArrayOf(1, 2, 3), "filename", "originalFileName", "application/pdf")
-        val exisingAppliedProposal = AppliedProposal( id = "1",
-                proposalId = proposalId,
-                studentId = studentId,
-                status = ApplicationStatus.PENDING, file = null)
 
-        `when`(appliedProposalRepository.findByProposalIdAndStudentId(proposalId,studentId)).thenReturn(exisingAppliedProposal)
-        val responseEntity = appliedProposalController.createApplyForProposal(proposalId,studentId,file)
+        `when`(appliedProposalService.applyForProposal(proposalId, studentId, file))
+                .thenReturn(ResponseEntity.status(HttpStatus.BAD_REQUEST).body("ERROR in creating the application (APPLICATION ALREADY EXISTS)."))
+
+        val responseEntity = appliedProposalController.createApplyForProposal(proposalId, studentId, file)
+
         assert(responseEntity.statusCode == HttpStatus.BAD_REQUEST)
         assert(responseEntity.body == "ERROR in creating the application (APPLICATION ALREADY EXISTS).")
-    }*/
+    }
+
+    @Test
+    fun testApplyForProposalNonExistingProposal() {
+        val nonExistingProposalId = "99"
+        val studentId = "student1"
+        val file = FileDTO(byteArrayOf(1, 2, 3), "filename", "originalFileName", "application/pdf")
+
+        `when`(appliedProposalService.applyForProposal(nonExistingProposalId, studentId, file))
+                .thenReturn(ResponseEntity.status(HttpStatus.BAD_REQUEST).body("ERROR in creating the application (PROPOSAL NOT PRESENT in the database)."))
+
+        val responseEntity = appliedProposalController.createApplyForProposal(nonExistingProposalId, studentId, file)
+
+        assert(responseEntity.statusCode == HttpStatus.BAD_REQUEST)
+        assert(responseEntity.body == "ERROR in creating the application (PROPOSAL NOT PRESENT in the database).")
+    }
+
+    @Test
+    fun testApplyForProposalNonExistingStudent() {
+        val proposalId = "1"
+        val nonExistingStudentId = "student99"
+        val file = FileDTO(byteArrayOf(1, 2, 3), "filename", "originalFileName", "application/pdf")
+
+        `when`(appliedProposalService.applyForProposal(proposalId, nonExistingStudentId, file))
+                .thenReturn(ResponseEntity.status(HttpStatus.BAD_REQUEST).body("ERROR in creating the application (STUDENT NOT PRESENT in the database)."))
+
+        val responseEntity = appliedProposalController.createApplyForProposal(proposalId, nonExistingStudentId, file)
+
+        assert(responseEntity.statusCode == HttpStatus.BAD_REQUEST)
+        assert(responseEntity.body == "ERROR in creating the application (STUDENT NOT PRESENT in the database).")
+    }
+
+    @Test
+    fun testApplyForProposalAcceptedOrPending() {
+        val existingProposalId = "1"
+        val studentId = "student1"
+        val file = FileDTO(byteArrayOf(1, 2, 3), "filename", "originalFileName", "application/pdf")
+
+        `when`(appliedProposalService.applyForProposal(existingProposalId, studentId, file))
+                .thenReturn(ResponseEntity.status(HttpStatus.BAD_REQUEST).body("ERROR in creating the application (STUDENT ALREADY HAS AN APPLICATION)."))
+
+        val responseEntity = appliedProposalController.createApplyForProposal(existingProposalId, studentId, file)
+
+        assert(responseEntity.statusCode == HttpStatus.BAD_REQUEST)
+        assert(responseEntity.body == "ERROR in creating the application (STUDENT ALREADY HAS AN APPLICATION).")
+    }
+
+    @Test
+    fun testApplyForProposalInternalError() {
+        val existingProposalId = "1"
+        val studentId = "student1"
+        val file = FileDTO(byteArrayOf(1, 2, 3), "filename", "originalFileName", "application/pdf")
+
+        `when`(appliedProposalService.applyForProposal(existingProposalId, studentId, file))
+                .thenReturn(ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Internal Server Error"))
+
+        val responseEntity = appliedProposalController.createApplyForProposal(existingProposalId, studentId, file)
+
+        assert(responseEntity.statusCode == HttpStatus.INTERNAL_SERVER_ERROR)
+        assert(responseEntity.body == "Internal Server Error")
+    }
 
     @Test
     fun testGetAppliedProposalByStudent() {
@@ -135,6 +280,31 @@ class AppliedProposalUnitTests {
 
         assert(responseEntity.statusCode == HttpStatus.NOT_FOUND)
         assert(responseEntity.body == "ERROR: this student NOT EXISTS")
+    }
+
+    @Test
+    fun testGetAppliedProposalByStudentServerError() {
+        val studentId = "existingStudentId"
+
+        `when`(appliedProposalService.appliesByStudentId(studentId))
+                .thenReturn(ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Internal Server Error"))
+
+        val responseEntity = appliedProposalController.getAppliedProposalByStudent(studentId)
+
+        assert(responseEntity.statusCode == HttpStatus.INTERNAL_SERVER_ERROR)
+        assert(responseEntity.body == "Internal Server Error")
+    }
+
+    @Test
+    fun testGetAppliedProposalByStudentEmptyList() {
+        val studentId = "studentWithNoApplications"
+
+        `when`(appliedProposalService.appliesByStudentId(studentId)).thenReturn(ResponseEntity.ok(emptyList<AppliedProposalDTO>()))
+
+        val responseEntity = appliedProposalController.getAppliedProposalByStudent(studentId)
+
+        assert(responseEntity.statusCode == HttpStatus.OK)
+        assert(responseEntity.body == emptyList<AppliedProposalDTO>())
     }
 
     @Test
@@ -165,6 +335,31 @@ class AppliedProposalUnitTests {
     }
 
     @Test
+    fun testGetAppliedProposalByProposalServerError() {
+        val proposalId = "existingProposalId"
+
+        `when`(appliedProposalService.appliesByStudentId(proposalId))
+                .thenReturn(ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Internal Server Error"))
+
+        val responseEntity = appliedProposalController.getAppliedProposalByStudent(proposalId)
+
+        assert(responseEntity.statusCode == HttpStatus.INTERNAL_SERVER_ERROR)
+        assert(responseEntity.body == "Internal Server Error")
+    }
+
+    @Test
+    fun testGetAppliedProposalByProposalEmptyList() {
+        val proposalId = "proposalWithNoApplications"
+
+        `when`(appliedProposalService.appliesByStudentId(proposalId)).thenReturn(ResponseEntity.ok(emptyList<AppliedProposalDTO>()))
+
+        val responseEntity = appliedProposalController.getAppliedProposalByStudent(proposalId)
+
+        assert(responseEntity.statusCode == HttpStatus.OK)
+        assert(responseEntity.body == emptyList<AppliedProposalDTO>())
+    }
+
+    @Test
     fun testAcceptProposal() {
         val applicationId = "1"
 
@@ -176,6 +371,84 @@ class AppliedProposalUnitTests {
 
         assert(responseEntity.statusCode == HttpStatus.OK)
         assert(responseEntity.body == "Successful operation")
+    }
+
+    @Test
+    fun testAcceptProposalNotFound() {
+        val applicationId = "nonExistingId"
+
+        `when`(appliedProposalService.acceptProposal(applicationId))
+                .thenReturn(ResponseEntity.status(HttpStatus.NOT_FOUND).body("ERROR: this Application NOT EXIST"))
+
+        val responseEntity = appliedProposalController.acceptProposal(applicationId)
+
+        assert(responseEntity.statusCode == HttpStatus.NOT_FOUND)
+        assert(responseEntity.body == "ERROR: this Application NOT EXIST")
+    }
+
+    @Test
+    fun testAcceptProposalAlreadyAccepted() {
+        val applicationId = "1"
+
+        `when`(appliedProposalService.acceptProposal(applicationId))
+                .thenReturn(ResponseEntity.status(HttpStatus.BAD_REQUEST).body("ERROR: this Application has already been ACCEPTED"))
+
+        val responseEntity = appliedProposalController.acceptProposal(applicationId)
+
+        assert(responseEntity.statusCode == HttpStatus.BAD_REQUEST)
+        assert(responseEntity.body == "ERROR: this Application has already been ACCEPTED")
+    }
+
+    @Test
+    fun testAcceptProposalAlreadyCancelled() {
+        val applicationId = "1"
+
+        `when`(appliedProposalService.acceptProposal(applicationId))
+                .thenReturn(ResponseEntity.status(HttpStatus.BAD_REQUEST).body("ERROR: this Application has already been CANCELLED"))
+
+        val responseEntity = appliedProposalController.acceptProposal(applicationId)
+
+        assert(responseEntity.statusCode == HttpStatus.BAD_REQUEST)
+        assert(responseEntity.body == "ERROR: this Application has already been CANCELLED")
+    }
+
+    @Test
+    fun testAcceptProposalAlreadyRejected() {
+        val applicationId = "1"
+
+        `when`(appliedProposalService.acceptProposal(applicationId))
+                .thenReturn(ResponseEntity.status(HttpStatus.BAD_REQUEST).body("ERROR: this Application has already been REJECTED"))
+
+        val responseEntity = appliedProposalController.acceptProposal(applicationId)
+
+        assert(responseEntity.statusCode == HttpStatus.BAD_REQUEST)
+        assert(responseEntity.body == "ERROR: this Application has already been REJECTED")
+    }
+
+    @Test
+    fun testAcceptProposalUnexpectedError() {
+        val applicationId = "1"
+
+        `when`(appliedProposalService.acceptProposal(applicationId))
+                .thenReturn(ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Unexpected Error"))
+
+        val responseEntity = appliedProposalController.acceptProposal(applicationId)
+
+        assert(responseEntity.statusCode == HttpStatus.INTERNAL_SERVER_ERROR)
+        assert(responseEntity.body == "Unexpected Error")
+    }
+
+    @Test
+    fun testAcceptProposalServerError() {
+        val applicationId = "1"
+
+        `when`(appliedProposalService.acceptProposal(applicationId))
+                .thenReturn(ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Internal Server Error"))
+
+        val responseEntity = appliedProposalController.acceptProposal(applicationId)
+
+        assert(responseEntity.statusCode == HttpStatus.INTERNAL_SERVER_ERROR)
+        assert(responseEntity.body == "Internal Server Error")
     }
 
     @Test
@@ -191,5 +464,109 @@ class AppliedProposalUnitTests {
         assert(responseEntity.statusCode == HttpStatus.OK)
         assert(responseEntity.body == "Successful operation")
     }
+
+    @Test
+    fun testRejectProposalNotFound() {
+        val applicationId = "nonExistingId"
+
+        `when`(appliedProposalService.rejectProposal(applicationId))
+                .thenReturn(ResponseEntity.status(HttpStatus.NOT_FOUND).body("ERROR: this Application NOT EXIST"))
+
+        val responseEntity = appliedProposalController.rejectProposal(applicationId)
+
+        assert(responseEntity.statusCode == HttpStatus.NOT_FOUND)
+        assert(responseEntity.body == "ERROR: this Application NOT EXIST")
+    }
+
+    @Test
+    fun testRejectProposalAlreadyAccepted() {
+        val applicationId = "1"
+
+        `when`(appliedProposalService.rejectProposal(applicationId))
+                .thenReturn(ResponseEntity.status(HttpStatus.BAD_REQUEST).body("ERROR: this Application has already been ACCEPTED"))
+
+        val responseEntity = appliedProposalController.rejectProposal(applicationId)
+
+        assert(responseEntity.statusCode == HttpStatus.BAD_REQUEST)
+        assert(responseEntity.body == "ERROR: this Application has already been ACCEPTED")
+    }
+
+    @Test
+    fun testRejectProposalAlreadyCancelled() {
+        val applicationId = "1"
+
+        `when`(appliedProposalService.rejectProposal(applicationId))
+                .thenReturn(ResponseEntity.status(HttpStatus.BAD_REQUEST).body("ERROR: this Application has already been CANCELLED"))
+
+        val responseEntity = appliedProposalController.rejectProposal(applicationId)
+
+        assert(responseEntity.statusCode == HttpStatus.BAD_REQUEST)
+        assert(responseEntity.body == "ERROR: this Application has already been CANCELLED")
+    }
+
+    @Test
+    fun testRejectProposalAlreadyRejected() {
+        val applicationId = "1"
+
+        `when`(appliedProposalService.rejectProposal(applicationId))
+                .thenReturn(ResponseEntity.status(HttpStatus.BAD_REQUEST).body("ERROR: this Application has already been REJECTED"))
+
+        val responseEntity = appliedProposalController.rejectProposal(applicationId)
+
+        assert(responseEntity.statusCode == HttpStatus.BAD_REQUEST)
+        assert(responseEntity.body == "ERROR: this Application has already been REJECTED")
+    }
+
+    @Test
+    fun testRejectProposalUnexpectedError() {
+        val applicationId = "1"
+
+        `when`(appliedProposalService.rejectProposal(applicationId))
+                .thenReturn(ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Unexpected Error"))
+
+        val responseEntity = appliedProposalController.rejectProposal(applicationId)
+
+        assert(responseEntity.statusCode == HttpStatus.INTERNAL_SERVER_ERROR)
+        assert(responseEntity.body == "Unexpected Error")
+    }
+
+    @Test
+    fun testRejectProposalServerError() {
+        val applicationId = "1"
+
+        `when`(appliedProposalService.rejectProposal(applicationId))
+                .thenReturn(ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Internal Server Error"))
+
+        val responseEntity = appliedProposalController.rejectProposal(applicationId)
+
+        assert(responseEntity.statusCode == HttpStatus.INTERNAL_SERVER_ERROR)
+        assert(responseEntity.body == "Internal Server Error")
+    }
+
+
+    /*
+    MANCANO TEST PER QUESTE APIs:
+    @GetMapping("/active/{professorId}")
+    fun getActiveByProfessorId (@PathVariable professorId: String) : ResponseEntity<Any> {
+        val proposalsWithApplications = appliedProposalService.findByProfessor( professorId, archiviation_type.NOT_ARCHIVED )
+        return ResponseEntity.ok(proposalsWithApplications)
+    }
+    @GetMapping("/archived/{professorId}")
+    fun getArchivedByProfessorId (@PathVariable professorId: String) : ResponseEntity<Any> {
+        val proposalsWithApplications = appliedProposalService.findByProfessor( professorId, archiviation_type.EXPIRED, archiviation_type.MANUALLY_ARCHIVED )
+        return ResponseEntity.ok(proposalsWithApplications)
+    }
+
+    @GetMapping("/active/{coSupervisorId}")
+    fun getActiveByCoSupervisorId (@PathVariable coSupervisorId: String) : ResponseEntity<Any> {
+        val proposalsWithApplications = appliedProposalService.findByCoSupervisor( coSupervisorId, archiviation_type.NOT_ARCHIVED )
+        return ResponseEntity.ok(proposalsWithApplications)
+    }
+    @GetMapping("/archived/{coSupervisorId}")
+    fun getArchivedCoSupervisorId (@PathVariable coSupervisorId: String) : ResponseEntity<Any> {
+        val proposalsWithApplications = appliedProposalService.findByCoSupervisor( coSupervisorId, archiviation_type.EXPIRED, archiviation_type.MANUALLY_ARCHIVED )
+        return ResponseEntity.ok(proposalsWithApplications)
+    }
+     */
 
 }
